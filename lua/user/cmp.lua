d601eb757193a836d8a38c212545beff885a8518
Patrick <patrick.lopdrup@gmail.com>
2022-10-05 11:20:13 +0200

    Add init file and lua folder

---
+++ lua/user/cmp.lua
@@ -0,0 +1,112 @@
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+    vim.notify("cmp not installed.")
+    return
+end
+
+local snip_status_ok, luasnip = pcall(require, "luasnip")
+if not snip_status_ok then
+    vim.notify("luasnip not installed")
+    return
+end
+
+require("luasnip/loaders/from_vscode").lazy_load()
+
+local check_backspace = function()
+    local col = vim.fn.col "." - 1
+    return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+end
+
+--   פּ ﯟ   some other good icons
+local kind_icons = {
+    Text = "",
+    Method = "m",
+    Function = "",
+    Constructor = "",
+    Field = "",
+    Variable = "",
+    Class = "",
+    Interface = "",
+    Module = "",
+    Property = "",
+    Unit = "",
+    Value = "",
+    Enum = "",
+    Keyword = "",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "",
+    Event = "",
+    Operator = "",
+    TypeParameter = "",
+}
+-- find more here: https://www.nerdfonts.com/cheat-sheet
+
+cmp.setup {
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body) -- For `luasnip` users.
+        end,
+    },
+    window = {
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expandable() then
+                luasnip.expand()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif check_backspace() then
+                fallback()
+            else
+                fallback()
+            end
+        end, {"i","s",}),
+    }),
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "nvim_lua" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+    }),
+    formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+            -- Kind icons
+            vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+            -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+            vim_item.menu = ({
+                nvim_lsp = "[LSP]",
+                nvim_lua = "[NVIM_LUA]",
+                luasnip = "[Snippet]",
+                buffer = "[Buffer]",
+                path = "[Path]",
+            })[entry.source.name]
+            return vim_item
+        end,
+    },
+    confirm_opts = {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = false,
+    },
+    experimental = {
+        ghost_text = true,
+        native_menu = false,
+    },
+}
---
+++ lua/user/options.lua
@@ -0,0 +1,48 @@
+-- Language
+vim.opt.langmenu = en_US
+vim.cmd "let $LANG = 'en_US'"
+
+vim.opt.mouse="a"
+vim.opt.tabstop=4 softtabstop=4 
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.nu = true
+vim.opt.fileencoding = "utf-8"
+vim.opt.errorbells = false
+vim.cmd "set guicursor=n-v-c:block-Cursor"
+vim.opt.hlsearch = false
+vim.opt.hidden = true
+vim.opt.wrap = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.autochdir = true
+
+-- To keep history
+vim.opt.swapfile = false
+vim.opt.termguicolors = true
+vim.opt.backup = false
+vim.cmd "set undodir=~/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.incsearch = true
+vim.opt.scrolloff=8
+vim.opt.signcolumn="yes"
+vim.opt.colorcolumn="80"
+vim.opt.cursorline = true
+vim.opt.clipboard="unnamedplus"
+
+--call plug#begin('~/vimfiles/plugged')
+--Plug 'nvim-telescope/telescope.nvim'
+--Plug 'gruvbox-community/gruvbox'
+--Plug 'ycm-core/YouCompleteMe'
+--call plug#end()
+
+--colorscheme gruvbox
+--highlight Normal guibg=none
+
+vim.mapleader = " "
+--vim.cmd "colorscheme = gruvbox"
+--nnoremap <leader>ps :lua require('telescope.builtin').grep_string({ search = vim.fn.input("Grep For > ")})<CR>
