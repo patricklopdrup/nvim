ab5e1cd340f768dcaa2ddc031bdd672566226d6a
Patrick <patrick.lopdrup@gmail.com>
2022-10-12 11:21:54 +0200

    Add telescope

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -50,3 +50,6 @@ keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
+-- Telescope -- 
+keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
+keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts) -- (ripgrep has to be installed) scoop install ripgrep
