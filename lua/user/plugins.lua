16429915777fe81cce16f26974e1ee0da3932113
Patrick <patrick.lopdrup@gmail.com>
2022-10-27 14:53:46 +0200

    Add treesitter highlighting

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -75,6 +75,13 @@ return packer.startup(function(use)
     use "nvim-telescope/telescope.nvim"
     use "nvim-telescope/telescope-media-files.nvim"
 
+    -- Treesitter
+    use {
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+    }
+    use "p00f/nvim-ts-rainbow"
+
     -- Markdown preview
     use({
         "iamcco/markdown-preview.nvim",
