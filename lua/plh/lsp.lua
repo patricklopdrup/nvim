1930d1e67cc81ea86368e06eef41839b2b000289
Patrick <patrick.lopdrup@gmail.com>
2023-02-22 16:48:55 +0100

    Disable Enter key from cmp completion

--- lua/plh/lsp.lua
+++ lua/plh/lsp.lua
@@ -26,10 +26,12 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
     ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
     ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
     ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+    -- Prevent Enter key from confirming
+    ["<CR>"]  = function (fallback)
+        fallback()
+    end,
     ["<C-Space>"] = cmp.mapping.complete(),
 })
-
-cmp_mappings["<Enter>"] = nil
 
 lsp.setup_nvim_cmp({
     mapping = cmp_mappings
